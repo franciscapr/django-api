751b4badbd254b66c5cfe198eddf26ea9bfaed8e
Francisca <francisca.paredes.pr@gmail.com>
2024-12-23 22:16:46 -0300

    model user

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'core.user',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Indicamos a Django que use este modelo de usuario para el modelo se usuario de autenticaciòn
+AUTH_USER_MODEL = 'core_user.User'
