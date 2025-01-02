beb3ac502ce8f37f90d0fc71486460e5ee659b01
Francisca <francisca.paredes.pr@gmail.com>
2025-01-02 11:30:51 -0300

    test post|model

--- core/post/tests.py
+++ core/post/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import pytest
+from core.fixtures.user import user
+from core.post.models import Post
 
-# Create your tests here.
+# Este archvios se encargarà de probar la creaciòn de un post.
+
+@pytest.mark.django_db
+def test_create_post(user):
+    post = Post.objects.create(author=user,
+                               body="Test Post Body")
+    assert post.body == "Test Post Body"
+    assert post.author == user
+
+
+
